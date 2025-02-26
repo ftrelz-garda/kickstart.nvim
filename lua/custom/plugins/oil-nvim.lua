f9e318ff1176e0809a4643d2307293de6c41f327
Fred Trelz <fred.trelz@garda.com>
2025-02-26 10:11:57 -0600

    added tmux-navigator, tabs as spaces, oil keybinds

--- lua/custom/plugins/oil-nvim.lua
+++ lua/custom/plugins/oil-nvim.lua
@@ -2,7 +2,13 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    keymaps = {
+      ['<C-h>'] = false,
+      ['<C-l>'] = false,
+      ['<C-r>'] = 'actions.refresh',
+    },
+  },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
--- lua/custom/sets.lua
+++ lua/custom/sets.lua
@@ -1,2 +1,8 @@
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
